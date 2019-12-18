4259ad2be833dcbb43c920e89b78a0f814f86aed
Totillity <39473801+Totillity@users.noreply.github.com>
2019-12-17 18:37:44 -0700

    Commit

--- gen_site.py
+++ gen_site.py
@@ -3,62 +3,8 @@ from typing import ClassVar
 
 import jinja2
 
-template = jinja2.Template("""
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Aize-lang</title>
-      <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="images/png" href="images/logo50x50.png">
-</head>
-<body>
-    <div class="side-bar">
-        <img src="images/logo.svg" alt="aize logo">
-        <div class="side-bar-title">
-            Navigation
-        </div>
-        <div class="side-bar-links">
-            <a href="#title" class="side-bar-link"><p>Aize</p></a>
-            {% for item in items %}
-                {% if item.type == "Header" %}
-                    <a href="#{{ item.anchor }}" class="side-bar-link"><p>{{ item.header }}</p></a>
-                {% endif %}
-            {% endfor %}
-        </div>
-    </div>
-    <div class="top-bar">
-        <p class="top-bar-title">The Aize Programming Language</p>
-    </div>
-    <div class="title-bar">
-        <a id="title"></a>
-        <div class="title-container">
-            <div class="title">
-                Aize
-            </div>
-            <div class="title-blurb">
-                The Future of Programming
-            </div>
-        </div>
-    </div>
-    <div class="content">
-        {% for item in items %}
-            {% if item.type == "Text" %}
-                <p class="text">{{ item.text }}</p>
-            {% elif item.type == "Image" %}
-                <div class="img-box">
-                    <img src="{{ item.image }}" alt="{{ item.alt }}">
-                    <p>{{ item.caption }}</p>
-                </div>
-            {% elif item.type == "Header" %}
-                <a id="{{ item.anchor }}" class="jump-target"></a>
-                <p class="header">{{ item.header }}</p>
-            {% endif %}
-        {% endfor %}
-    </div>
-</body>
-</html>
-""")
+with open("template/index.html") as template_file:
+    template = jinja2.Template(template_file.read())
 
 
 @dataclass()
@@ -98,6 +44,7 @@ def main(items):
     file = template.render(items=items)
     with open("index.html", "w") as index:
         index.write(file)
+        print("written")
 
 
 main([
